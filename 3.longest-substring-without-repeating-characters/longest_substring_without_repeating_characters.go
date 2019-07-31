5a6293edc58ca5cf605d8ba0fba73d7d92bffb3e
iwwxiong <iwwxiong@gmail.com>
2019-07-31 14:59:25 +0800

    3.longest_substring_without_repeating_characters

--- 3.longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.go
+++ 3.longest-substring-without-repeating-characters/longest_substring_without_repeating_characters.go
@@ -2,9 +2,19 @@ package algorithms3
 
 func lengthOfLongestSubstring(s string) int {
 	var start = -1
-	var tempMap = make(map[]int)
-	var subLen = 0
+	var tempMap = make(map[string]int)
+	var maxLen = 0
 	for i, chr := range s {
-
+		index, ok := tempMap[string(chr)]
+		if ok {
+			if start < index {
+				start = index
+			}
+		}
+		if i-start > maxLen {
+			maxLen = i - start
+		}
+		tempMap[string(chr)] = i
 	}
+	return maxLen
 }
--- 3.longest-substring-without-repeating-characters/longest_substring_without_repeating_characters_test.go
+++ 3.longest-substring-without-repeating-characters/longest_substring_without_repeating_characters_test.go
@@ -11,11 +11,16 @@ func Test_lengthOfLongestSubstring(t *testing.T) {
 		args args
 		want int
 	}{
-	// TODO: Add test cases.
+		// TODO: Add test cases.
 		{"a->1", args{"a"}, 1},
+		{"aab->2", args{"aab"}, 2},
+		{"dvdf->3", args{"dvdf"}, 3},
+		{"abba->2", args{"abba"}, 2},
+		{"abcd->4", args{"abcd"}, 4},
 		{"bbbbb->1", args{"bbbbb"}, 1},
 		{"abcabcbb->3", args{"abcabcbb"}, 3},
 		{"abcdac->4", args{"abcdac"}, 4},
+		{"pwwkew->3", args{"pwwkew"}, 3},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
