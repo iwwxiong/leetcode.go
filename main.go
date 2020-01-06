227f86e78edd99bcf5f6c98bde45817e64456b05
iwwxiong <iwwxiong@gmail.com>
2020-01-06 18:56:51 +0800

    longest common prefix accepted.
    Runtime: 0 ms, faster than 100.00% of Go online submissions for Longest Common Prefix.
    Memory Usage: 2.4 MB, less than 100.00% of Go online submissions for Longest Common Prefix.

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	fmt.Println(11 / 2)
-	fmt.Println(11 % 2)
+	// fmt.Println(11 / 2)
+	// fmt.Println(11 % 2)
 	// s := "1234567890"
 	// fmt.Println(s[5:10])
 	// fmt.Println(int32(-654) % 10)
@@ -21,6 +21,6 @@ func main() {
 	// fmt.Printf("%v\n", c)
 	// fmt.Println(math.MaxInt32)
 	// fmt.Println(math.MinInt32)
-	b := "a"[1:]
+	b := "abc"[:3]
 	fmt.Println(b)
 }
